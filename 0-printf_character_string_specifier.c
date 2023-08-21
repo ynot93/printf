c83bb029b46a56519f674fc881e4d7a47027fc1c
UniterRiziki <mpekethuuniter@gmail.com>
2023-08-21 23:46:57 +0300

    Print characters and strings

---
+++ 0-printf_character_string_specifier.c
@@ -0,0 +1,59 @@
+#include "main.h"
+/**
+ *_printf-function that executes printf
+ *@format: string to be formatted
+ *
+ *Return: the number of characters
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count;
+	const char *str;
+
+	va_start(args, format);
+	count = 0;
+	
+	while (*format)
+	{	
+		if (*format == '%')
+		{
+			format++;
+			
+			switch (*format)
+			{
+				case 'c':
+					putchar(va_arg(args, int));
+					count++;
+					break;
+				case 's':
+					str = va_arg(args, const char *);
+					while(*str)
+					{
+						putchar(*str);
+						count++;
+					}
+					break;
+				case '%':
+					putchar('%');
+					count++;
+					break;
+				default:
+					putchar('%');
+					putchar(*format);
+					count += 2;
+					break;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count ++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}				
+					
