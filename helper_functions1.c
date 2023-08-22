8c6e9dc0f3320057682f617be8a6aba832c56440
UniterRiziki <mpekethuuniter@gmail.com>
2023-08-23 00:17:05 +0300

    Add functionality for handling binary

--- helper_functions1.c
+++ helper_functions1.c
@@ -11,3 +11,30 @@ int _print_char(va_list args)
 	putchar(va_arg(args, int));
 	return (1);
 }
+/**
+ * handle_binary - converts integer to binary number
+ * @args: list of arguments.
+ *
+ * Return: The number of characters
+ */
+int handle_binary(va_list args)
+{
+	int number = va_arg(args, int);
+	int count = 0;
+	int binary[32];
+	int num_bits = 0;
+
+	do {
+		binary[num_bits++] = number % 2;
+		number /= 2;
+	} while (number > 0);
+
+	while (num_bits > 0)
+	{
+		num_bits--;
+		putchar(binary[num_bits] + '0');
+		count++;
+	}
+
+	return (count);
+}
