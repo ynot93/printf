b0e74229c37bb523e9a64297dc5b4081e7628ff1
Tony Mputhia <tonymmputhia@gmail.com>
2023-08-23 15:29:23 +0300

    Fix buffer functionality

--- _printf.c
+++ _printf.c
@@ -1,47 +1,34 @@
 #include "main.h"
 /**
- * _printf-function that executes printf
- * @format: string to be formatted
+ * _printf - Function that executes printf.
+ * @format: String to be formatted.
  *
- * Return: the number of characters
+ * Return: The number of characters.
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count, buffer_index, i;
-	char buffer[BUFFER_SIZE];
+	int count;
 
 	va_start(args, format);
 	count = 0;
-	buffer_index = 0;
 
 	if (format == NULL)
 		return (-1);
 
 	while (*format)
 	{
-		if (*format == '%')
+		if (*format != '%')
+		{
+			putchar(*format);
+			count++;
+		}
+		else
 		{
 			format++;
 			count += dispatcher(*format, args);
 		}
-		else
-		{
-			buffer[buffer_index++] = *format;
-			count++;
-		}
-		if (buffer_index >= BUFFER_SIZE - 1)
-		{
-			for (i = 0; i < buffer_index; i++)
-				putchar(buffer[i]);
-			buffer_index = 0;
-		}
 		format++;
-	}
-	if (buffer_index > 0)
-	{
-		for (i = 0; i < buffer_index; i++)
-			putchar(buffer[i]);
 	}
 	va_end(args);
 	return (count);
